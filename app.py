64721bdaea1ea41c9988550d8395e98f9697f4c2
Anurag Choudhary <anuragcho25@gmail.com>
2025-04-19 12:22:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@
     "colab": {
       "provenance": [],
       "authorship_tag": "ABX9TyNaj6AySWoc0bvYywfPr5Lp",
-      "include_colab_link": true
+      "include_colab_link": True
     },
     "kernelspec": {
       "name": "python3",
